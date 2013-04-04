2fc76de0cc20cc27769c8171f5f385b82fb30d9b
Jen Lim <jennolim@gmail.com>
2013-04-04 17:00:37 -0400

    Remove side search box

--- movie_server.rb
+++ movie_server.rb
@@ -1,15 +1,48 @@
 require 'sinatra'
+#require 'rack-flash'
+require 'pry'
 
 require_relative 'movies'
 
+
+before '/' do 
+	unless params[:password] == "coolbeans"
+	#	flash[:notice] = "Sorry, wrong password"
+		redirect '/login'
+	end
+end
+
+
 get '/' do
-  # Just have a nice welcome page
+  erb :index
+end
+
+post '/login' do
+  erb :index
 end
 
 post '/film' do
   # Search for a Movie
   # HINT - what is in params ?
-  Movie.get_film_info(name)
+ 
+  movie = Movie.get_film_info(params[:film])
+  puts movie 
 
-  # Display the movie in the page
+  erb :film, :locals => {:film => movie}
+
+
 end
+
+get '/login' do
+  erb :login
+end
+
+# get '/output' do
+
+# 	#film = params[:film]
+# 	raise movie.inspect
+# 	erb :output, :locals => {:film => movie}
+
+# 	#binding.pry
+
+# end
--- movies.rb
+++ movies.rb
@@ -6,10 +6,25 @@ class Movie
   # Add attr_accessors for the values you want to store...
   attr_accessor :title, :year, :rating, :genre, :director, :actors, :plot, :poster_url
 
+  # def initialize(name)
+  #   m = Movie.new
+  #   m.title = movie_info["Title"]
+  #   m.year = movie_info["Year"]
+  #   m.rating = movie_info["Rating"]
+  #   m.genre = movie_info["Genre"]
+  #   m.director = movie_info["Director"]
+  #   m.actors = movie_info["Actors"].split(",")
+  #   m.plot = movie_info["Plot"]
+  #   m.poster_url = movie_info["Poster"]
+  #   m
+  # end
+
   def self.get_film_info(name)    
     imdb_data = HTTParty.get("http://www.omdbapi.com/?t=#{name}")
     movie_info = JSON(imdb_data)
+
     
+  #   puts movie_info
     # Create a Movie object...
     m = Movie.new
     m.title = movie_info["Title"]
